c7a3e303acc2ae1aa044932b798f3d4f9d1d6d52
michaelcheah <michaelcheah95@gmail.com>
2018-03-13 16:40:58 +0000

    Training Scripts Added

--- generate_test.py
+++ generate_test.py
@@ -6,18 +6,21 @@ from Training.Data import Data
 import numpy as np
 
 class Model:
-    def eval(self, board):
+    def __init__(self, size, input_moves):
+        self.size = size
+        self.input_moves = input_moves
+    def eval(self, boards, next_player):
         P = np.ones(26)/26
         V = 0.1
         return P, V
 
 # Starting player
-model = Model()
+model = Model(5, 4)
 player = BLACK 
 
 trainer = Data(model)
 
-trainingset = trainer.Generate(num_samples=4)
+trainingset, piset = trainer.generate(num_samples=10)
 
 print (np.shape(trainingset))
 print (trainingset[-1])
---
+++ overall.py
@@ -0,0 +1,31 @@
+from Selfplay import Selfplay
+from GoGame.GoSimulator import GoSimulator
+from Shared.Consts import BLACK, WHITE
+from Training.Data import Data
+
+from Model.model import Model
+
+import numpy as np
+
+# Set Constants
+SIZE = 5
+INPUT_MOVES = 3
+PLAYER = BLACK
+NUM_SAMPLES = 100
+
+# Initialize Model
+go_model = Model(SIZE, INPUT_MOVES)
+
+# Initialize Trainer
+data = Data(go_model, player=PLAYER, size=SIZE, input_moves=INPUT_MOVES)
+
+# Generate 100 number of samples
+print ("Generating Sample Games")
+training_set, pi_set, black_leads_set = data.generate(num_samples=6)
+
+print ("Fitting Model")
+go_model.model.fit(training_set, [pi_set, black_leads_set], epochs=1, batch_size=2)
+
+# print (np.shape(trainingset))
+# print (trainingset[-1])
+# print (np.shape(piset))
