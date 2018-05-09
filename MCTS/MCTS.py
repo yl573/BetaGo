291c1a095c5d26a337ec8b7bbc71b94bd15f7d2a
yl573 <yl573@cam.ac.uk>
2018-05-09 21:07:36 +0100

    corrected end state value to -1 and 1, was 0 and 1

--- MCTS/MCTS.py
+++ MCTS/MCTS.py
@@ -58,11 +58,11 @@ def process_end_state(boards, player):
     game.set_board_from_prev_boards(boards, player)
     black_lead = game.black_score_lead()
     if black_lead == 0:
-        V = 0.5
+        V = 0
     elif player == BLACK:
-        V = black_lead > 0
+        V = (black_lead > 0)*2 - 1
     else:
-        V = black_lead < 0
+        V = (black_lead < 0)*2 - 1
     P = np.zeros(26)
     return P, V
 
