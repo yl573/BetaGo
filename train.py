db2bd25a7ec6ff0076a2ab184d4be96611121496
yl573 <yl573@cam.ac.uk>
2018-05-16 21:26:41 +0100

    updated requirements.txt

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import sys
 sys.stdout = writer('train.log', sys.stdout)
 
 trainer = Trainer(
-    model_file='checkpoint.h5', 
+    model_file='best_model.h5', 
     benchmark_file='best_model.h5',
     search_iters=110, 
     cpuct=1
@@ -13,10 +13,10 @@ trainer = Trainer(
 
 for i in range(10):
     trainer.play_games_and_train(
-        num_games=10, 
+        num_games=20, 
         win_thresh=0.65, 
         verbose=0, 
-        epochs=1, 
+        epochs=2, 
         save_name='game_data/data_16_3_' + str(i) + '.pkl'
     )
 
